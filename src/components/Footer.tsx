5b767db6b9d01ab255931d6d1a833dd983f22385
cjephuneh <calebjephuneh@gmail.com>
2024-12-10 15:47:52 +0300

    Refactor image size in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="space-y-4">
             <div className="flex items-center space-x-2 mb-6">
-              <img src={footerImage} alt="Envisage Multi Media" className="h-64 w-64 sm:h-10 sm:w-10" />
+              <img src={footerImage} alt="Envisage Multi Media" className="h-auto w-64 sm:h-10 sm:w-10" />
             </div>
             <p className="text-gray-400 text-sm sm:text-base">
               Transforming healthcare delivery through innovative technology solutions.
